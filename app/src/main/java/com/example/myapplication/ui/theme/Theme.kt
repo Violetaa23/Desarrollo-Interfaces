cfd475e4f11439610756c27005f4b59e04980789
DESKTOP-SNU5A6J\Violeta <violeta.uriengr@elorrieta-errekamari.com>
2024-11-14 11:45:22 +0100

    Plantilla Compose

--- app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun MyApplicationTheme(
+fun Plantilla_ComposeTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
